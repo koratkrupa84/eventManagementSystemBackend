3b52ab3008dd7836826dffdc158473bf8c3d1906
parasprajapati641 <parasprajapati641@gmail.com>
2026-09-02 16:16:56 +0530

    (2/9/2026) upadate

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 function dbConnect() {
-  mongoose.connect("mongodb+srv://Krupa:08032006@cluster0.vsou0cr.mongodb.net/eventManagement")
+  mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ MongoDB connected successfully"))
     .catch((err) => {
       console.error("❌ MongoDB connection error:", err);
